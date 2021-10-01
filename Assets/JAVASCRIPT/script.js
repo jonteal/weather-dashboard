07896ce9e4accceb3dea752004cdfaa48b76d753
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-30 22:09:17 -0500

    Created Variables for "City" and "queryURL"

--- Assets/JAVASCRIPT/script.js
+++ Assets/JAVASCRIPT/script.js
@@ -1,2 +1,10 @@
 var APIKey = "6a495eb658d2f860658cf774331a385d";
+var city;
+var state;
+var country;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+api.openweathermap.org/data/2.5/weather?q=city&appid=APIKey
+
+
+fetch(queryURL)
