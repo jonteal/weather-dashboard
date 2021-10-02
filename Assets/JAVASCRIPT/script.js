6141fca427bf403489b043b7d1fe4ead9bc5196a
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-02 17:44:45 -0500

    Main Element Initial Setup Done

--- Assets/JAVASCRIPT/script.js
+++ Assets/JAVASCRIPT/script.js
@@ -1,10 +1,14 @@
 var APIKey = "6a495eb658d2f860658cf774331a385d";
 var city;
-var state;
-var country;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 api.openweathermap.org/data/2.5/weather?q=city&appid=APIKey
 
 
 fetch(queryURL)
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+
+// 5 day weather forecast API
+api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=5&appid=APIKey
