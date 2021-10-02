0ee30fdc6d77af1008cf4f136a5be935ae1ed44d
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-02 18:37:04 -0500

    Added fetchWeather function

--- Assets/JAVASCRIPT/script.js
+++ Assets/JAVASCRIPT/script.js
@@ -11,4 +11,19 @@ https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={par
 
 
 // 5 day weather forecast API
-api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=5&appid=APIKey
+// api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=5&appid=APIKey;
+
+var searchBtn = document.getElementById("search-btn");
+
+let weather = {
+    "apiKey": "6a495eb658d2f860658cf774331a385d"
+    fetchWeather: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=Fayetteville&units=imperial&appid=6a495eb658d2f860658cf774331a385d"
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+};
+
+
